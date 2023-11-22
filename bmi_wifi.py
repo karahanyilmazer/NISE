46aafc3d70f573ca043c850067e51dc76e55a286
muhammed_gorunen <ge94soc@mytum.de>
2023-11-22 23:36:48 +0100

    Socket programming added

--- bmi_wifi.py
+++ bmi_wifi.py
@@ -3,13 +3,36 @@ import socket
 import time
 
 import serial
-from shared_memory_dict import SharedMemoryDict
+#from shared_memory_dict import SharedMemoryDict
 
 # %%
 # # Function for sending command in binary mode
 def send_command(command):
     # Send the command over the socket connection
     arduino_sock.sendall(command.encode())
+
+import socket
+
+# Set up the server
+int_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.2'  # Change this to the IP address of your server
+port = 12345         # Choose a port number
+
+int_socket.bind((host, port))
+int_socket.listen(1)
+
+print(f"Server listening on {host}:{port}")
+
+# Accept a connection
+client_socket, client_address = int_socket.accept()
+print(f"Connection from {client_address}")
+
+# Send a message to the client
+#message_to_client = "Hello, client! How are you?"
+
+
+
+
 
 
 # Replace with the IP address of ESP32
@@ -31,12 +54,12 @@ port = serial.Serial('/dev/ttyUSB0', baudrate=115200)  # Linux
 
 
 # Shared memory for communicating between different scripts
-smd = SharedMemoryDict(name='msg', size=1024)
-smd['sensor_1'] = 0
-smd['sensor_2'] = 0
-smd['sensor_3'] = 0
-smd['sensor_4'] = 0
+#smd = SharedMemoryDict(name='msg', size=1024)
+#smd['sensor'] = 0
+#smd['sending'] = False
+
 print('flamina')
+shooter = 0
 # %%
 
 while True:
@@ -60,16 +83,43 @@ while True:
         # sensorValue1, sensorValue2, sensorValue3, sensorValue4 = 0,0,0,0
         sensorVal = [0, 0, 0, 0]
 
-    sender = '0'
+    interm = '0'
     for i, sensorV in enumerate(sensorVal):
-        if int(sensorV) > 1500:
-            sender = str(i + 1)
-            smd[f'sensor_{i + 1}'] = 1
+        if int(sensorV) > 2500:
+            interm = str(i + 1)
+
+        
+
+    # past_ind = interm
+    # counter = counter +1
+
+    if shooter != 0:
+        if int(interm) == 0:
+            shooter = 0
         else:
-            smd[f'sensor_{i + 1}'] = 0
+            sender = 0
 
-    #send_command(sensorValue1, sensorValue2, sensorValue3, sensorValue4)
-    send_command(sender)
-    print(sender)
+    else:
+        if int(interm) != 0:
+            sender = int(interm)
+            shooter = 1
+        else:
+            sender = 0
+            shooter = 0
 
+    #smd['sensor'] = sender
+
+    
+    #if smd['sending'] == True:
+    ##  send_command(sensorValue1, sensorValue2, sensorValue3, sensorValue4)
+    #    send_command(sender)
+        
+    if sender != 0:
+        client_socket.sendall(sender.to_bytes(4, byteorder='big'))
+        print(sender)
+
+
+# Close the sockets
+client_socket.close()
+int_socket.close()
 # %%
