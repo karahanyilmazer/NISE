d53a388586219c265463483980756ff8e928d781
Karahan Yilmazer <karahan.yilmazer@tum.de>
2023-11-22 18:36:10 +0100

    feat: Add shared memory dictionary value sharing

--- bmi_wifi.py
+++ bmi_wifi.py
@@ -1,42 +1,48 @@
 # %%
-
 import socket
 import time
+
 import serial
 from shared_memory_dict import SharedMemoryDict
 
 # %%
-
-# Function for sending command in binary mode
-def send_command(command):
-    arduino_sock.sendall(command.encode())  # Send the command over the socket connection
+# # Function for sending command in binary mode
+# def send_command(command):
+#     # Send the command over the socket connection
+#     arduino_sock.sendall(command.encode())
 
 
 # Replace with the IP address of ESP32
-arduino_host = "192.168.43.223"
+arduino_host = '192.168.43.223'
 # Replace with the port number used for Arduino communication
 arduino_port = 25002
 
 # Create socket connection
-arduino_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-arduino_sock.connect((arduino_host, arduino_port))
+# arduino_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# arduino_sock.connect((arduino_host, arduino_port))
 
 # Wait for game initialization
 time.sleep(3)
 
-port = serial.Serial("COM7", baudrate=115200)  # Windows
+# Set up serial connection port
+port = serial.Serial('COM5', baudrate=115200)  # Windows
+
+# Shared memory for communicating between different scripts
+smd = SharedMemoryDict(name='msg', size=1024)
+smd['sensor_1'] = 0
+smd['sensor_2'] = 0
+smd['sensor_3'] = 0
+smd['sensor_4'] = 0
 
 # %%
-
 while True:
     try:
-        line = port.readline().decode("utf-8")
+        line = port.readline().decode('utf-8')
     except KeyboardInterrupt:
         break
 
     # Split the line into individual sensor values
-    sensor_values = line.split(",")
+    sensor_values = line.split(',')
     try:
         sensorValue1, sensorValue2, sensorValue3, sensorValue4 = sensor_values
         # Convert to integer if necessary
@@ -49,13 +55,16 @@ while True:
         # sensorValue1, sensorValue2, sensorValue3, sensorValue4 = 0,0,0,0
         sensorVal = [0, 0, 0, 0]
 
-    sender = "0"
+    sender = '0'
     for i, sensorV in enumerate(sensorVal):
         if int(sensorV) > 1500:
-            sender = str(i+1)
-    
+            sender = str(i + 1)
+            smd[f'sensor_{i + 1}'] = 1
+        else:
+            smd[f'sensor_{i + 1}'] = 0
+
     # send_command(sensorValue1, sensorValue2, sensorValue3, sensorValue4)
-    send_command(sender)
-    print(sender)
+    # send_command(sender)
+    # print(sender)
 
 # %%
